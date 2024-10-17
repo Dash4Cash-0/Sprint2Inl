3f8627e21a3089b05e7329bccb5c245b4a937cad
Kim Zugic <kim.zugic@live.com>
2024-10-17 20:28:53 +0200

    initial commit

---
+++ src/BestGymEver.java
@@ -0,0 +1,15 @@
+public class BestGymEver {
+
+
+
+
+    public void mainProgram(){
+
+
+    }
+
+    public static void main(String[] args) {
+        BestGymEver start = new BestGymEver();
+        start.mainProgram();
+    }
+}
