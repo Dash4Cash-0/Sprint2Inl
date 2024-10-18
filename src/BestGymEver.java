ed32a0099775ac1232a9461350ebf61adb01fabb
Kim Zugic <kim.zugic@live.com>
2024-10-18 22:16:07 +0200

    some code for test run

--- src/BestGymEver.java
+++ src/BestGymEver.java
@@ -6,6 +6,19 @@ public class BestGymEver {
     public void mainProgram(){
         MemberData md = new MemberData();
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        System.out.println(md.searchForMemberByName("Bear"));
+        try{
+            if(md.searchForMemberByPersonalNumber("8512021234") == null){
+
+            }else{
+                System.out.println(md.searchForMemberByPersonalNumber("8512021234"));
+            }
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("Not a member");
+        }
+
+
     }
 
     public static void main(String[] args) {
