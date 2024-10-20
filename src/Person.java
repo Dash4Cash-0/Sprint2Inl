f3950d4f396ea588a9651367a72c9217150e70ff
Kim Zugic <kim.zugic@live.com>
2024-10-20 11:45:37 +0200

    added instance variables

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,23 @@
 import java.time.LocalDate;
 
 public class Person {
-    public Person(String name, String personalNumber, LocalDate lastPayment){
+    private String name;
+    private String pNumber;
+    private LocalDate trainingDate;
 
+    public Person(String name, String personalNumber, LocalDate trainingDate){
+        this.name = name;
+        this.pNumber = personalNumber;
+        this.trainingDate = trainingDate;
     }
+    public String getName(){
+        return name;
+    }
+    public String getPersonalNumber(){
+        return pNumber;
+    }
+    public LocalDate getTrainingDate(){
+        return trainingDate;
+    }
+
 }
