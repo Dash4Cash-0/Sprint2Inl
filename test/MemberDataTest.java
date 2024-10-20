54a0d82eb52fc135e3e4fceecff8d00a14c5fa36
Kim Zugic <kim.zugic@live.com>
2024-10-20 11:46:10 +0200

    added if member test

--- test/MemberDataTest.java
+++ test/MemberDataTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.Assert.*;
 
@@ -53,20 +56,17 @@ public class MemberDataTest {
     }
 
     @Test
-    public void checkIfMember() {
+    public void checkIfMemberTest() {
+        String test1 = "2019-12-02";
+        String test2 = "2023-12-02";
+        LocalDate testDate1 = LocalDate.parse(test1);
+        boolean year1 = ChronoUnit.YEARS.between(testDate1, LocalDate.now()) <= -1;
+        LocalDate testDate2 = LocalDate.parse(test2);
+        boolean year2 = ChronoUnit.YEARS.between(testDate2, LocalDate.now()) > -1;
 
-        try(BufferedReader br = Files.newBufferedReader(testFile)){
-            String line;
 
-            while((line = br.readLine()) != null){
-                if(line.contains("2019-12-02")){
-                    break;
-                }
-            }
-        }catch (IOException e){
-            System.out.println("File not found");
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        assertTrue(md.checkIfMember(test1) == year1);
+        assertTrue(md.checkIfMember(test2) == year2);
+        assertFalse(md.checkIfMember(test1) == year2);
     }
 }
