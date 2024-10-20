cd556f8aaf7da2c6f87cde8e950f1b2146fc8b56
Kim Zugic <kim.zugic@live.com>
2024-10-20 11:46:33 +0200

    write to file method

--- src/MemberData.java
+++ src/MemberData.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 
@@ -13,7 +17,6 @@ public class MemberData {
 
     Path ptFile = Paths.get("src/PTFile.txt");
     Path memberFile = Paths.get("src/members.txt");
-
 
 
     public String readFile(String input){
@@ -36,7 +39,7 @@ public class MemberData {
         return search;
     }
 
-    public LocalDate getLastPayment(String input){
+    public String getLastPayment(String input){
         String date = "";
 
         try(BufferedReader br = Files.newBufferedReader(memberFile)){
@@ -46,8 +49,6 @@ public class MemberData {
                 if(line.contains(input)){
                     date = br.readLine();
                     break;
-                } else {
-                    System.out.println("Not a member");
                 }
             }
         }catch (IOException e){
@@ -55,7 +56,7 @@ public class MemberData {
         }catch (Exception e){
             e.printStackTrace();
         }
-        return LocalDate.parse(date);
+        return date;
     }
 
 
@@ -70,4 +71,25 @@ public class MemberData {
 
         return memberInfo.substring(0,10);
     }
+
+    public Boolean checkIfMember(String lastPayment){
+        return ChronoUnit.YEARS.between(LocalDate.parse(lastPayment), LocalDate.now()) < 1;
+    }
+
+    public void writeToFile(Person person){
+        try(FileWriter fw = new FileWriter(String.valueOf(ptFile), true);
+            BufferedWriter bw = new BufferedWriter(fw)){
+
+                if(!Files.exists(ptFile)){
+                Files.createFile(ptFile);
+            }
+            bw.write(person.getName() + ", ");
+            bw.write(person.getPersonalNumber() + ", ");
+            bw.write(person.getTrainingDate().toString());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
