b6ef62a579e414f3f8a4c9de5170472f0f9419cc
Kim Zugic <kim.zugic@live.com>
2024-10-20 21:52:49 +0200

    added while loop

--- src/BestGymEver.java
+++ src/BestGymEver.java
@@ -9,31 +9,37 @@ public class BestGymEver {
         Scanner input = new Scanner(System.in);
         LocalDate today = LocalDate.now();
 
-        System.out.println("Search for name or personalnumber: ");
-        String search = input.nextLine();
-
-        try {
-            if (search.matches("^[0-9]+$")) {
-                if (md.searchForMemberByPersonalNumber(search) != null) {
-                    checkPayment(md, today, search);
-                }
+        while (true) {
+            System.out.println("Search for name or personalnumber: ('exit' to quit program)");
+            String search = input.nextLine();
+            if (search.equals("exit")) {
+                System.out.println("Exiting program...");
+                System.exit(0);
             }
-            if (search.matches("^[a-zA-Z]+$")) {
+
+            try {
+                if (search.matches("^[0-9]+$")) {
+                    if (md.searchForMemberByPersonalNumber(search) != null) {
+                        checkPayment(md, today, search);
+                    }
+                }
+                if (search.matches("^[a-zA-Z]+$")) {
                     if (md.searchForMemberByName(search) != null) {
                         checkPayment(md, today, search);
                     }
+                }
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("Not a member");
             }
-        } catch (StringIndexOutOfBoundsException e){
-            System.out.println("Not a member");
         }
     }
 
     private void checkPayment(MemberData md, LocalDate today, String search) {
         if(!md.checkIfMember(md.getLastPayment(search))) {
-            System.out.println("It was more than a year since you payed for the membership");
+            System.out.println("It was more than a year since you payed for the membership \n");
         }else{
             System.out.println("Welcome!");
-            System.out.println(md.searchForMemberByName(search));
+            System.out.println(md.searchForMemberByName(search) + "\n");
 
             Person member = new Person(md.searchForMemberByName(search),
                     md.searchForMemberByPersonalNumber(search), today);
